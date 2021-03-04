0f9b24b134a3ecb60e5b13f67e7a82616e740dd5
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-03-04 22:32:06 +0100

    loading stylesheets

--- front-page.php
+++ front-page.php
@@ -1,3 +1,12 @@
 <?php get_header(); ?>
-coool
+
+<div class="container">
+  <div class="row">
+    <div class="col-12 pt-2">
+    <div class="container p-5 bg-dark">
+      <h2 class="text-light">Testing Bootstrap</h2>
+    </div>
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
