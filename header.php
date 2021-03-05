c37805bf2b991ef05cb79d9281b733640ec53b6a
lucasnevespereira <pereiraneveslucas@gmail.com>
2021-03-05 09:52:53 +0100

    added basic style to top-menu

--- header.php
+++ header.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head();?>
 </head>
 <body <?php body_class();?>>
 <header class="sticky-top">
-  <?php 
-    wp_nav_menu( 
-      array(
-        'theme_location' => 'top-menu'
-      )
-    );
-  ?>
+  <div class="container">
+    <?php 
+      wp_nav_menu( 
+        array(
+          'theme_location' => 'top-menu',
+          'menu_class' => 'navigation'
+        )
+      );
+    ?>
+  </div>
 </header>
+
+
   
